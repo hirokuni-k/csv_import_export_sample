c73674582fe83d6618beb40d16d4abb562d07f7c
hirokuni-k <shisenramen@yahoo.co.jp>
2021-04-13 16:28:04 +0900

    CSVインポート機能（リファクタリング）

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   CSV_COLUMNS = %w[name age height].freeze
   def self.import_csv(file:)
-    User.transaction do
-      CSV.foreach(file.path, headers: true) do |row|
-        create!(row.to_h.slice(*CSV_COLUMNS))
-      end
+    list = []
+    CSV.foreach(file.path, headers: true) do |row|
+      list << row.to_h.slice(*CSV_COLUMNS)
     end
+    User.import!(list)
   end
 end
